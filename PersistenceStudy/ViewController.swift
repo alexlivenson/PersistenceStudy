0b4a4d657343bb506716c4dad7a4a040e73629e0
alex livenson <alexlivenson1@alexs-macbook-pro.local>
2017-03-01 10:13:10 -0800

    Initial Commit

---
+++ PersistenceStudy/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PersistenceStudy
+//
+//  Created by alex livenson on 3/1/17.
+//  Copyright © 2017 alex livenson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
